01aba36f980f3202bfd813571a14c4e08ca51da5
Marc Kalmes <mkalmes@mkalmes.net>
2022-04-28 09:02:33 +0200

    Fix syntax

--- finicky.js
+++ finicky.js
@@ -1,11 +1,13 @@
 module.exports = {
   defaultBrowser: "Safari",
   handlers: [
-    match: [
-      "mail.google.com/*",
-      "meet.google.com/*",
-      "calendar.google.com/*"
-    ],
-    browser: "Google Chrome"
+    {
+      match: [
+        "mail.google.com/*",
+        "meet.google.com/*",
+        "calendar.google.com/*"
+      ],
+      browser: "Google Chrome"
+    }
   ]
 };
