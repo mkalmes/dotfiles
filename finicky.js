7dc06280ddcd6e3ef293e7e8f95f26488d852926
Marc Kalmes <mkalmes@mkalmes.net>
2022-04-28 08:39:14 +0200

    Add config for Finicky

---
+++ finicky.js
@@ -0,0 +1,11 @@
+module.exports = {
+  defaultBrowser: "Safari",
+  handlers: [
+    match: [
+      "mail.google.com/*",
+      "meet.google.com/*",
+      "calendar.google.com/*"
+    ],
+    browser: "Google Chrome"
+  ]
+};
