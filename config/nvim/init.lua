ce119304d1486faf8ab7c24fd94d4a91e38ad323
Marc Kalmes <mkalmes@mkalmes.net>
2025-01-25 08:16:34 +0100

    Add initial nvim config

---
+++ config/nvim/init.lua
@@ -0,0 +1,18 @@
+print("Hello World")
+
+-- Kudos to TJ DeVries
+-- https://www.youtube.com/watch?v=CuWfgiwI73Q
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
+vim.keymap.set("n", "<space>x", ":.lua<CR>")
+vim.keymap.set("v", "<space>x", ":lua<CR>")
+
+-- Highlight when yanking (copying) text
+--  Try it with `yap` in normal mode
+--  See `:help vim.highlight.on_yank()`
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
