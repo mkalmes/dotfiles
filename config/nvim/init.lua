c7a3131369bfbbd92719dec7fe7da987967fe2f4
Marc Kalmes <mkalmes@mkalmes.net>
2025-01-25 15:00:18 +0100

    Disable cursor keys

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -8,6 +8,13 @@ vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
 vim.keymap.set("v", "<space>x", ":lua<CR>")
 
+-- Hard mode
+for _, mode in pairs({'n', 'i'}) do
+  vim.keymap.set(mode, "<Up>", "<Nop>")
+  vim.keymap.set(mode, "<Down>", "<Nop>")
+  vim.keymap.set(mode, "<Left>", "<Nop>")
+  vim.keymap.set(mode, "<Right>", "<Nop>")
+end
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
